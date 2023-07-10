7faa7992277f0c731119681032a95769829f0313
kiatklai <likhitthaworn.k@gmail.com>
2023-07-10 20:26:26 +0900

    try to make component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//create Component
+// function HelloComponent(){
+//   return<h1>Say Hi to Component</h1>
+// }
+class HelloComponent extends React.Component{
+  render(){
+    return <h1> Hello react</h1>
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HelloComponent/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
