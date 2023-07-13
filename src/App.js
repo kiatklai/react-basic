6b95aeb91213290b07720e7e1e60bdc6e8489a45
kiatklai <likhitthaworn.k@gmail.com>
2023-07-13 17:28:22 +0900

    収入支出アプリ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Transaction from "./component/Transaction";
 import './App.css';
 import FormComponent from "./component/FormComponent";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import DataContext from "./data/DataContext";
 import Reportcomponent from "./component/ReportComponent";
 
@@ -9,16 +9,28 @@ const Title =()=><h1 style={{color:"red",textAlign:"center",fontSize:"2rem"}}>å
 
 function App() {
   const [items,setItem] = useState([])
+
+  const [reportIncome,setReportIncome] = useState(0)
+  const [reportExpense,setReportExpense] = useState(0)
+
   const onAddNewItem = (newItem) =>{
     setItem((prevItem)=>{
       return [newItem,...prevItem]
     })
   }
+  useEffect(()=>{
+    const amounts = items.map(items=>items.amount)
+    const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
+    const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
+    setReportIncome(income)
+    setReportExpense(expense)
+  },[items,reportIncome,reportExpense])
+
   return (
     <DataContext.Provider value={
       {
-        income: 50000,
-        expense: -8000
+        income: reportIncome,
+        expense: reportExpense
       }
     }>
       <div className="container">
--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,11 +1,8 @@
-import { useContext } from 'react'
 import Item from './Item'
 import './transaction.css'
-import DataContext from '../data/DataContext'
 
 const Transaction =(props)=>{
   const {items} = props
-  const {income , expense} = useContext(DataContext)
   return (
     <div>
       <ul className='item-list'>
@@ -13,8 +10,6 @@ const Transaction =(props)=>{
           return <Item {...element} key={element.id}/>
         })}
       </ul>
-      <p>収入：{income}</p>
-      <p>支出：{expense}</p>
     </div>
   );
 }
