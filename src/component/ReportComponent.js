81bf9a826a2c0dc20e3d9c590cbe7a792dbbac1d
kiatklai <likhitthaworn.k@gmail.com>
2023-07-15 17:27:53 +0900

    format number

--- src/component/ReportComponent.js
+++ src/component/ReportComponent.js
@@ -4,18 +4,21 @@ import './ReportComponent.css'
 
 const Reportcomponent=()=>{
   const {income , expense} = useContext(DataContext)
+  const formatNumber=(num)=> {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
   return (
     <div>
       <h4>残高（円）</h4>
-      <h1>{(income-expense).toFixed(2)}</h1>
+      <h1>{formatNumber((income-expense).toFixed(2))}</h1>
       <div className="report-container">
         <div>
           <h4>総収入</h4>
-          <p className="report plus">￥{income}</p>
+          <p className="report plus">￥{formatNumber(income)}</p>
         </div>
         <div>
           <h4>総支出</h4>
-          <p className="report minus">￥{expense}</p>
+          <p className="report minus">￥{formatNumber(expense)}</p>
         </div>
       </div>
     </div>
